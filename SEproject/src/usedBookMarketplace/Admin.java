505d95bf4c6b09e00e7ea2cb4bb6005534dee6a8
seungyeon <seongyeon38@naver.com>
2021-02-17 16:52:15 +0900

    Delete SEproject/src directory

--- SEproject/src/usedBookMarketplace/Admin.java
+++
@@ -1,9 +0,0 @@
-package usedBookMarketplace;
-
-public class Admin  {
-	private String adminId = "admin";
-	private String adminPassword = "nayana";
-
-	public String getAdminID() {return adminId;}
-	public String getAdminPassword() {return adminPassword;}
-}
